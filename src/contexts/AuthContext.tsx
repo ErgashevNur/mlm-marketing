82ba4c4cfd4699856a795c3890ad59032844572e
CodeNur <dodgechalanger9@gmail.com>
2025-06-17 18:52:18 +0500

    refactor: uncomment token handling in RegisterPage and update loginWithGoogle method

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -158,9 +158,9 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   const loginWithGoogle = () => {
     window.location.href = "https://mlm-backend.pixl.uz/authorization/google";
 
-    // const url = new URL(window.location.href);
-    // const token = url.searchParams.get("token");
-    // console.log(token);
+    const url = new URL(window.location.href);
+    const token = url.searchParams.get("token");
+    console.log(token);
   };
 
   const loginWithFacebook = async () => {
