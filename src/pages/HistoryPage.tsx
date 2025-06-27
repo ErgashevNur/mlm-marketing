01898e3534e0d6e98d8977121e8eb7e5108f909d
r <r>
2025-06-27 22:06:03 +0500

    R

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -138,13 +138,16 @@ export default function BonusHistory() {
     const token = localStorage.getItem("token");
 
     try {
-      const response = await fetch("/product-history/user", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_KEY}/product-history/user`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (!response.ok) {
         throw new Error("Serverdan xatolik: " + response.status);
