69c615c33ad337b40a730217f28d0ca753126b91
r <r>
2025-07-02 20:02:16 +0500

    z

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // SPA routing uchun muhim
-  build: {
-    outDir: "dist", // build papka nomi
-    chunkSizeWarningLimit: 1000, // ogohlantirish chegarasi
-  },
-  server: {
-    port: 3000,     // dev server porti
-    open: true,     // avtomatik browser ochiladi
+  optimizeDeps: {
+    exclude: ["lucide-react"],
   },
 });
